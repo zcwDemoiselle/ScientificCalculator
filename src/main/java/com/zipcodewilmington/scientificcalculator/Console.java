9850938382ba819773f538e91002d155dcf936f8
Gowri <gowri@ZipcodersMBP64.local>
2023-10-28 20:09:26 -0400

    Updated for ScientificCalculator

--- src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
@@ -18,15 +18,43 @@ public class Console {
     public static String getStringInput(String prompt) {
         Scanner scanner = new Scanner(System.in);
         println(prompt);
-        String userInput = scanner.nextLine();
-        return userInput;
+        String userStrInput = scanner.nextLine();
+        return userStrInput;
     }
 
+
     public static Integer getIntegerInput(String prompt) {
-        return null;
+        Scanner scanner = new Scanner(System.in);
+        println(prompt);
+        int userIntInput = scanner.nextInt();
+        return userIntInput;
     }
 
     public static Double getDoubleInput(String prompt) {
-        return null;
+        Scanner scanner = new Scanner(System.in);
+        println(prompt);
+        double userDoubleInput = (int) scanner.nextDouble();
+        return userDoubleInput;
+    }
+
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public static double square(double a) {
+        return a * a;
+    }
+
+    public static double squareRoot(double a){
+        return Math.sqrt(a);
+    }
+
+    public static double inverse(double a) {
+        return 1 / a;
+    }
+
+    public static double switchSign(double a) {
+        return -1 * a ;
+
     }
 }
--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -1,10 +1,13 @@
 package com.zipcodewilmington.scientificcalculator;
 
+import java.util.Scanner;
+
 /**
  * Created by leon on 2/9/18.
  */
 public class MainApplication {
     public static void main(String[] args) {
+
         Console.println("Welcome to my calculator!");
         String s = Console.getStringInput("Enter a string");
         Integer i = Console.getIntegerInput("Enter an integer");
@@ -14,7 +17,11 @@ public class MainApplication {
         Console.println("The user input %s as a integer", i);
         Console.println("The user input %s as a d", d);
 
-
+        Console.println("The result of subtracting " + d + " from " + i + " is : " + Console.subtract(i, d));
+        Console.println("The result of square is : "  + Console.square(i));
+        Console.println("The result of square root is : " + Console.squareRoot(i));
+        Console.println("The result of Inverse is : " + Console.inverse(i));
+        Console.println("The result of switch sign is : " + Console.switchSign(i));
 
         // Are we ready ?
     }
