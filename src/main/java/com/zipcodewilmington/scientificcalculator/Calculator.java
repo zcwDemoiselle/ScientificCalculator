7625ff23701c9cbd958f9bf6963b518314662958
Louie <louie@Zipcoders-MBP-3.fios-router.home>
2023-10-29 22:46:33 -0400

    Updated UML and made variables consistent

--- src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
@@ -3,47 +3,47 @@ package com.zipcodewilmington.scientificcalculator;
 import java.lang.Math;
 
 public class Calculator {
-    public double performUnaryOperation(double num1, String operator) {
+    public double performUnaryOperation(double number1, String operator) {
         switch (operator) {
             case "log(":
-                return Math.log(num1); // Logarithm with base 10
+                return Math.log(number1); // Logarithm with base 10
             case "sqrt(":
-                return Math.sqrt(num1);
+                return Math.sqrt(number1);
             //case "h":
             case "help":
                 Console.showHelp();
             case "!":
-                return Console.factorial(num1);
+                return Console.factorial(number1);
             case "1/x":
-                return Console.inverse(num1);
+                return Console.inverse(number1);
             case "-x":
-                return Console.switchSign(num1);
+                return Console.switchSign(number1);
             default:
                 throw new IllegalArgumentException("Invalid operator: " + operator);
         }
     }
-    public double performBinaryOperation(double num1, double num2, String operator) {
+    public double performBinaryOperation(double number1, double number2, String operator) {
         switch (operator) {
             case "+":
-                return num1 + num2;
+                return number1 + number2;
             case "-":
-                return num1 - num2;
+                return number1 - number2;
             case "*":
-                return Console.multiplication(num1, num2);
+                return Console.multiplication(number1, number2);
             case "/":
-                if (num2 != 0) {
-                    return num1 / num2;
+                if (number2 != 0) {
+                    return number1 / number2;
                 } else {
                     throw new ArithmeticException("Division by zero is not allowed.");
                 }
             case "%":
-                return Console.modulus(num1, num2);
+                return Console.modulus(number1, number2);
             case "^":
-                return Math.pow(num1, num2);
+                return Math.pow(number1, number2);
             case "log(":
-                return Math.log(num2) / Math.log(num1); // Logarithm with base num1
+                return Math.log(number2) / Math.log(number1); // Logarithm with base num1
             case "!":
-                return Console.factorial(num1);
+                return Console.factorial(number1);
             default:
                 throw new IllegalArgumentException("Invalid operator: " + operator);
         }
